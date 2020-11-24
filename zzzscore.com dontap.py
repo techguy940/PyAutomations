f1dcff4d67c89b671b2172f31294ab7cc41fa8ad
techguy940 <51826754+techguy940@users.noreply.github.com>
2020-11-24 17:28:11 +0530

    Update zzzscore.com dontap.py

--- zzzscore.com dontap.py
+++ zzzscore.com dontap.py
@@ -2,12 +2,16 @@ import pyautogui
 import keyboard
 import time
 
+# LINK: https://zzzscore.com/dontap/en/
+
 COORDS = [
-		(1006, 665),
-		(1133, 665),
-		(888, 665),
-		(778, 665)
-		]
+	(1006, 665),
+	(1133, 665),
+	(888, 665),
+	(778, 665)
+	]
+
+# COORDS MAY DIFFER FOR YOUR PC, pyautogui.displayMousePosition() to get coords with RGB
 
 while not keyboard.is_pressed('q'):
 	for i in COORDS:
